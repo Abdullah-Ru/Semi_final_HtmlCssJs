635aa1090eef2a9a556d81b8280522054dd78781
Abdullah <a.az119@hotmail.com>
2023-08-25 01:55:13 +0300

    Add login page

---
+++ JS/Login.js
@@ -0,0 +1,25 @@
+let Login = document.getElementById("Login")
+Login.addEventListener("submit", function Log(event) {
+    event.preventDefault();
+    let username = document.getElementById("username").value;
+    let password = document.getElementById("password").value;
+    if (username == "عبدالله" && password == "123123" || username == "غالي" && password == "1212" || username == "بتال" && password == "1111") {
+        localStorage.setItem("username", username)
+        setTimeout(function() {
+            window.location.href = "12.html"
+        }, 3000);
+        Swal.fire({
+            title: "! تم تسجيل دخولك بنجاح",
+            text: "... يتم تحويلك الى الصفحة الرئيسية",
+            background: "#DDB892",
+            allowOutsideClick : false,
+            confirmButtonText: "حسنًا",
+            icon: "success",
+            confirmButtonColor:"#9C6644",
+
+        })
+    } else {
+        alert("غير صحيح حاول مره اخرى")
+    }
+
+})
